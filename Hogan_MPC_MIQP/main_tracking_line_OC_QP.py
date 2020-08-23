252fb056a2124c187e96756c08630e50cb3a0871
Joao Moura <joaopousa24@gmail.com>
2020-08-23 16:08:14 +0100

    Working nlp

--- Hogan_MPC_MIQP/main_tracking_line_OC_QP.py
+++ Hogan_MPC_MIQP/main_tracking_line_OC_QP.py
@@ -10,6 +10,7 @@
 ## Import Libraries
 #  -------------------------------------------------------------------
 import numpy as np
+import numpy.matlib as nplib
 from scipy.integrate import dblquad 
 import casadi as cs
 import matplotlib.pyplot as plt
@@ -23,12 +24,15 @@ g = 9.81 # gravity acceleration constant in meter per second square
 a = 0.09 # side dimension of the square slider in meters
 m = 0.827 # mass of the slider in kilo grams
 miu_g = 0.35 # coeficient of friction between slider and table
+miu_p = 0.3 # coeficient of friction between pusher and slider
 T = 5 # time of the simulation is seconds
-N = 50 # numer of increments per second
+freq = 50 # numer of increments per second
 r_pusher = 0.01 # radious of the cilindrical pusher in meter
 #  -------------------------------------------------------------------
 ## Computing Problem constants
 #  -------------------------------------------------------------------
+N = T*freq # total number of iterations
+h = 1./freq # time interval of each iteration
 A = a**2 # area of the slider in meter square
 f_max = miu_g*m*g # limit force in Newton
 # Area integral of norm of the distance for a square:
@@ -71,51 +75,121 @@ f = cs.SX(cs.vertcat(cs.mtimes(cs.mtimes(R,L),cs.mtimes(B,u[0:2])),u[2]))
 f_func = cs.Function('f', [x,u], [f])
 #  -------------------------------------------------------------------
 
+## Compute Jacobians
+#  -------------------------------------------------------------------
+A = cs.jacobian(f, x)#[0:3,0:3]
+A_func = cs.Function('A', [x,u], [A])
+B = cs.jacobian(f, u)#[0:3,0:2]
+B_func = cs.Function('B', [x], [B])
+#  -------------------------------------------------------------------
+
 ## Generate Nominal Trajectory (line)
 #  -------------------------------------------------------------------
 # constant input and initial state
-u_const = [0.03, 0, 0]
-x0 = [0, 0, 0, 0]
+u0 = cs.SX(3,1); u0[0] = 0.03
+x0 = cs.SX(4,1)
 #  -------------------------------------------------------------------
-t = cs.SX.sym('t')
-dae = {'x':x, 't':t, 'ode': f_func(x, u_const)}
-ts = np.linspace(0, T, T*N)
+t = cs.SX.sym('t'); ts = np.linspace(0, T, N)
+dae = {'x':x, 't':t, 'ode': f_func(x, u0)}
 F = cs.integrator('F', 'cvodes', dae, {'grid':ts, 'output_t0':True})
-sol = F(x0=x0)
-x_nom = np.array(F(x0=x0)['xf'])
+X_nom = np.array(F(x0=cs.DM(x0).full())['xf'])
+U_nom = cs.repmat(u0, 1, N)
+#  -------------------------------------------------------------------
+
+## Set up QP Optimization Problem
+#  -------------------------------------------------------------------
+## ---- Input variables ---
+X_bar = cs.SX.sym('x_bar', 4, N)
+U_bar = cs.SX.sym('u_bar', 3, N)
+## ---- Optimization objective ----------
+Qcost = 10*cs.diag(cs.SX([3,3,0.1,0]))
+QNcost = 2000*cs.diag(cs.SX([3,3,0.1,0]))
+Rcost = 0.5*cs.diag(cs.SX([1,1,0.01]))
+Cost = cs.dot(X_bar[:,-1],cs.mtimes(QNcost,X_bar[:,-1]))
+for i in range(N-1):
+    Cost += cs.dot(X_bar[:,i],cs.mtimes(Qcost,X_bar[:,i])) + cs.dot(U_bar[:,i],cs.mtimes(Rcost,U_bar[:,i]))
+## ---- Initialize variables for optimization problem ---
+w=[]
+w0=[]
+g = []
+lbg = []
+ubg = []
+## ---- Initial Conditions ----
+# ... 
+# put here the starting state
+## ---- Dynamic constraints ----
+for i in range(N-1):
+    Ai = A_func(X_nom[:,i], U_nom[:,i])
+    Bi = B_func(X_nom[:,i])
+    g += [X_bar[:,i+1]-h*(cs.mtimes(Ai,X_bar[:,i])-cs.mtimes(Bi,U_bar[:,i]))]
+    lbg += [0, 0, 0, 0]
+    ubg += [0, 0, 0, 0]
+## ---- Control constraints ----
+for i in range(N):
+    # normal force
+    g += [(U_bar[0,i]+U_nom[0,i])]
+    lbg += [0]
+    ubg += [cs.inf]
+    # tangential force
+    g += [miu_p*(U_bar[0,i]+U_nom[0,i])-cs.fabs(U_bar[1,i]+U_nom[1,i])]
+    lbg += [0]
+    ubg += [cs.inf]
+    # relative sliding velocity
+    g += [U_bar[2,i]+U_nom[2,i]]
+    lbg += [0]
+    ubg += [0]
+## ---- Setting up optimization variables ---
+for i in range(N):
+    w += [X_bar[:,i]]
+    w0 += [X_nom[:,i]]
+    w += [U_bar[:,i]]
+    w0 += [0, 0, 0]
+## ---- Create solver ----
+prob = {'f': Cost, 'x': cs.vertcat(*w), 'g': cs.vertcat(*g)}
+solver = cs.nlpsol('solver', 'ipopt', prob);
+## ---- Solve optimization problem ----
+sol = solver(x0=cs.vertcat(*w0), lbg=lbg, ubg=ubg)
+w_opt = sol['x']
+X_bar_opt = cs.vertcat(w_opt[0::7].T,w_opt[1::7].T,w_opt[2::7].T,w_opt[3::7].T)
+U_bar_opt = cs.vertcat(w_opt[4::7].T,w_opt[5::7].T,w_opt[6::7].T)
+X_opt = X_bar_opt + X_nom
+U_opt = U_bar_opt + U_nom
+print(X_opt.shape)
+print(U_opt.shape)
+print(X_opt[:,0])
 #  -------------------------------------------------------------------
 
 # Animation of Nominal Trajectory
 #  -------------------------------------------------------------------
-# set up the figure and subplot
-fig = plt.figure()
-fig.canvas.set_window_title('Matplotlib Animation')
-ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, \
-        xlim=(-0.1,0.5), ylim=(-0.1,0.5) \
-)
-ax.plot(x_nom[0,:], x_nom[1,:], color='red', linewidth=2.0, linestyle='dashed')
-ax.plot(x_nom[0,0], x_nom[1,0], x_nom[0,-1], x_nom[1,-1], marker='o', color='red')
-ax.grid(); ax.set_axisbelow(True)
-ax.set_title('Pusher-Slider Motion Animation')
-slider = patches.Rectangle(x0[0:2]-np.array([a/2, a/2]), a, a, x0[2])
-pusher = patches.Circle(np.array(p_pusher_func(x0)), radius=r_pusher, color='black')
-def init():
-    ax.add_patch(slider)
-    ax.add_patch(pusher)
-    return []
-    #return slider,
-def animate(i, slider, pusher):
-    slider.set_xy([x_nom[0,i]-a/2, x_nom[1,i]-a/2])
-    pusher.set_center(np.array(p_pusher_func(x_nom[:,i])))
-    return []
-# call the animation
-ani = animation.FuncAnimation(fig, animate, init_func=init, \
-        fargs=(slider,pusher,),
-        frames=N*T,
-        interval=T,
-        blit=True, repeat=False)
-## to save animation, uncomment the line below:
-## ani.save('sliding_nominal_traj.mp4', fps=50, extra_args=['-vcodec', 'libx264'])
-#show the animation
-plt.show()
+## # set up the figure and subplot
+## fig = plt.figure()
+## fig.canvas.set_window_title('Matplotlib Animation')
+## ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, \
+##         xlim=(-0.1,0.5), ylim=(-0.1,0.5) \
+## )
+## ax.plot(X_nom[0,:], X_nom[1,:], color='red', linewidth=2.0, linestyle='dashed')
+## ax.plot(X_nom[0,0], X_nom[1,0], X_nom[0,-1], X_nom[1,-1], marker='o', color='red')
+## ax.grid(); ax.set_axisbelow(True)
+## ax.set_title('Pusher-Slider Motion Animation')
+## slider = patches.Rectangle(x0[0:2]-np.array([a/2, a/2]), a, a, x0[2])
+## pusher = patches.Circle(np.array(p_pusher_func(x0)), radius=r_pusher, color='black')
+## def init():
+##     ax.add_patch(slider)
+##     ax.add_patch(pusher)
+##     return []
+##     #return slider,
+## def animate(i, slider, pusher):
+##     slider.set_xy([X_nom[0,i]-a/2, X_nom[1,i]-a/2])
+##     pusher.set_center(np.array(p_pusher_func(X_nom[:,i])))
+##     return []
+## # call the animation
+## ani = animation.FuncAnimation(fig, animate, init_func=init, \
+##         fargs=(slider,pusher,),
+##         frames=N,
+##         interval=T,
+##         blit=True, repeat=False)
+## ## to save animation, uncomment the line below:
+## ## ani.save('sliding_nominal_traj.mp4', fps=50, extra_args=['-vcodec', 'libx264'])
+## #show the animation
+## plt.show()
 #  -------------------------------------------------------------------
