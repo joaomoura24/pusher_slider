30b2035c1cea784a2bb67eddc213de63b2daebd7
Joao Moura <joaopousa24@gmail.com>
2021-01-26 17:15:00 +0000

    Forgot to add changes to my plot in prev commit

--- Hogan_MPC_MIQP/my_plots.py
+++ Hogan_MPC_MIQP/my_plots.py
@@ -23,16 +23,17 @@ def get_patches_for_square_slider_and_cicle_pusher(ax, pfunc, Rfunc, x_data, sq_
     d0 = np.array(cs.mtimes(Rfunc(x0),[-sq_sd/2, -sq_sd/2, 0]).T)[0]
     slider = patches.Rectangle(x0[0:2]+d0[0:2], sq_sd, sq_sd, x0[2])
     pusher = patches.Circle(np.array(pfunc(x0)), radius=r_pusher, color='black')
-    path, = ax.plot(x0[0], x0[1], color='orange')
+    path_past, = ax.plot(x0[0], x0[1], color='orange')
+    path_future, = ax.plot(x0[0], x0[1], color='orange', linestyle='dashed')
     ax.add_patch(slider)
     ax.add_patch(pusher)
-    path.set_linewidth(2)
-    return slider, pusher, path
+    path_past.set_linewidth(2)
+    return slider, pusher, path_past, path_future;
 #  -------------------------------------------------------------------
 
 ## Return patches
 #  -------------------------------------------------------------------
-def animate_square_slider_and_circle_pusher(i, slider, pusher, ax, pfunc, Rfunc, x_data, sq_sd, path=None):
+def animate_square_slider_and_circle_pusher(i, slider, pusher, ax, pfunc, Rfunc, x_data, sq_sd, path_past=None, path_future=None, X_future=None):
     xi = x_data[:,i]
     # distance between centre of square reference corner
     di = np.array(cs.mtimes(Rfunc(xi),[-sq_sd/2, -sq_sd/2, 0]).T)[0]
@@ -47,8 +48,10 @@ def animate_square_slider_and_circle_pusher(i, slider, pusher, ax, pfunc, Rfunc,
     slider.set_xy([ci[0], ci[1]])
     pusher.set_center(np.array(pfunc(xi)))
     # Set path changes
-    if path is not None:
-        path.set_data(x_data[0,0:i],x_data[1,0:i])
+    if path_past is not None:
+        path_past.set_data(x_data[0,0:i],x_data[1,0:i])
+    if (path_future is not None) and (X_future is not None):
+        path_future.set_data(X_future[0,:,i], X_future[1,:,i])
     return []
 #  -------------------------------------------------------------------
 
