1e0d0842164038b34ca4575dbee46ebdd8c7cbc6
Chris Mower <chris.mower@ed.ac.uk>
2021-09-07 20:52:23 +0100

    Use keyboard when joystick not available, also clean code

--- teleop/demo.py
+++ teleop/demo.py
@@ -9,7 +9,8 @@ from math import sin, cos
 from pygame_teleop.screen import Screen
 from pygame_teleop.joystick import Joystick
 
-"""Requires a joystick with at least 3 axes: hori/vert/rot"""
+"""Needs a joystick with at least 3 axes: hori/vert/rot. Otherwise,
+otherwise the keyboard is used."""
 
 numpy.set_printoptions(precision=2, suppress=True)
 
@@ -20,13 +21,13 @@ pi = numpy.pi
 mu_g = 0.5  # coefficient of friction between the object and the ground
 m = 1.0  # mass of object in kg
 g = 9.807  # gravitational acceleration
-box_width = 0.3  # width of box - along x axis
-box_height = 0.2  # height of box - along y axis
+slider_width = 0.3  # width of slider - along x axis
+slider_height = 0.2  # height of slider - along y axis
 
 # Compute constants
-A = box_width*box_height
+A = slider_width*slider_height  # area of slider
 fmax = mu_g*m*g
-mmax = (mu_g*m*g/A)*dblquad(lambda y, x: numpy.sqrt(x**2+y**2), -0.5*box_width, 0.5*box_width, -0.5*box_height, 0.5*box_height)[0]
+mmax = (mu_g*m*g/A)*dblquad(lambda y, x: numpy.sqrt(x**2+y**2), -0.5*slider_width, 0.5*slider_width, -0.5*slider_height, 0.5*slider_height)[0]
 L = numpy.diag([1/fmax**2, 1/fmax**2, 1/mmax**2])
 
 # Setup system dynamics
@@ -35,7 +36,6 @@ side2vec = dict(
     top = (numpy.array([0, -1], dtype=float), numpy.array([1, 0], dtype=float)),
     left = (numpy.array([1, 0], dtype=float), numpy.array([0, 1], dtype=float)),
     bottom = (numpy.array([0, 1], dtype=float), numpy.array([-1, 1], dtype=float)),
-
 )
 
 def xdot(x, u, r, side):
@@ -68,12 +68,12 @@ def xdot(x, u, r, side):
 
 class Point:
 
-    def __init__(self, x, y, box, mass=1.0):
+    def __init__(self, x, y, slider, mass=1.0):
         self.pos = numpy.asarray([x, y], dtype=float)
         self.vel = numpy.zeros(2)
         self.acc = None
-        self.box = box
-        p = self.pos_in_box_frame()
+        self.slider = slider
+        p = self.pos_in_slider_frame()
         self.angle = numpy.arctan2(p[1], p[0])
         self.angle_vel = None
         self.m = mass
@@ -87,7 +87,7 @@ class Point:
         self.force = self.acc * self.m
 
         old_angle = self.angle
-        p = self.pos_in_box_frame()
+        p = self.pos_in_slider_frame()
         new_angle = numpy.arctan2(p[1], p[0])
         if sign(old_angle) == 1 and sign(new_angle) == -1:
             new_angle += 2*pi
@@ -100,17 +100,17 @@ class Point:
     def pos_in_world_frame(self):
         return self.pos
 
-    def pos_in_box_frame(self):
-        R = Rotation.from_euler('z', self.box.heading, degrees=False).as_matrix()[:2,:2]
-        r = R.T@(self.pos - self.box.pos)
+    def pos_in_slider_frame(self):
+        R = Rotation.from_euler('z', self.slider.heading, degrees=False).as_matrix()[:2,:2]
+        r = R.T@(self.pos - self.slider.pos)
         return r
 
-    def force_in_box_frame(self):
-        R = Rotation.from_euler('z', self.box.heading, degrees=False).as_matrix()[:2,:2]
+    def force_in_slider_frame(self):
+        R = Rotation.from_euler('z', self.slider.heading, degrees=False).as_matrix()[:2,:2]
         return R.T@self.force
 
 
-class Box:
+class Slider:
 
     contact_tol = 0.01
 
@@ -120,7 +120,7 @@ class Box:
         self.width = width
         self.height = height
 
-        # left/top/right/bottom in box frame
+        # left/top/right/bottom in slider frame
         self.left = -0.5*width
         self.top = 0.5*height
         self.right = 0.5*width
@@ -131,7 +131,7 @@ class Box:
         self.heading = heading
 
     def near_point(self, point):
-        p = point.pos_in_box_frame()
+        p = point.pos_in_slider_frame()
         x = numpy.clip(p[0], self.left, self.right)
         y = numpy.clip(p[1], self.bottom, self.top)
         dl = abs(x-self.left)
@@ -140,20 +140,20 @@ class Box:
         db = abs(y-self.bottom)
         m = min(dl, dr, dt, db)
         if m == dt:
-            pos_in_box_frame = numpy.array([x, self.top])
+            pos_in_slider_frame = numpy.array([x, self.top])
             side = 'top'
         elif m == db:
-            pos_in_box_frame = numpy.array([x, self.bottom])
+            pos_in_slider_frame = numpy.array([x, self.bottom])
             side = 'bottom'
         elif m == dl:
-            pos_in_box_frame = numpy.array([self.left, y])
+            pos_in_slider_frame = numpy.array([self.left, y])
             side = 'left'
         else:
-            pos_in_box_frame = numpy.array([self.right, y])
+            pos_in_slider_frame = numpy.array([self.right, y])
             side = 'right'
         R = Rotation.from_euler('z', self.heading, degrees=False).as_matrix()[:2,:2]
-        in_contact = numpy.linalg.norm(pos_in_box_frame - p) <= self.contact_tol
-        return self.pos + R@pos_in_box_frame, side, in_contact
+        in_contact = numpy.linalg.norm(pos_in_slider_frame - p) <= self.contact_tol
+        return self.pos + R@pos_in_slider_frame, side, in_contact
 
 
 # Main program
@@ -196,49 +196,70 @@ def main():
     clock = pygame.time.Clock()
     draw_colors = ['red', 'green', 'blue']
     draw_color = None
-    box = Box(0.3, 0.2, 0, box_width, box_height)
-    pusher = numpy.array([0.25*box_width, box.bottom])
+    slider = Slider(0.3, 0.2, 0, slider_width, slider_height)
+    pusher = numpy.array([0.25*slider_width, slider.bottom])
     pusher_angle = numpy.arctan2(pusher[1], pusher[0])
     side = 'bottom'
     fmax = 1.5
     max_avel = numpy.deg2rad(10)
-    joy = Joystick()
+    try:
+        joy = Joystick()
+        use_joy = True
+    except pygame.error:
+        use_joy = False
+        j = [0.0, 0.0, 0.0, 0.0]
     running = True
 
     # Main loop
     try:
         while running:
 
-            # Check with user
+            # Update user input j
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        j[3] = -1.0
+                    elif event.key == pygame.K_RIGHT:
+                        j[3] = 1.0
+                    elif event.key == pygame.K_UP:
+                        j[1] = -1.0
+                    elif event.key == pygame.K_z:
+                        j[0] = -1.0
+                    elif event.key == pygame.K_x:
+                        j[0] = 1.0
+                elif event.type == pygame.KEYUP:
+                    if (event.key == pygame.K_LEFT) or (event.key == pygame.K_RIGHT):
+                        j[3] = 0.0
+                    elif event.key == pygame.K_UP:
+                        j[1] = 0.0
+                    elif (event.key == pygame.K_z) or (event.key == pygame.K_x):
+                        j[0] = 0.0
 
-            joy.reset()
-            j = joy.get_axes()
+            if use_joy:
+                joy.reset()
+                j = joy.get_axes()
 
+            # Update pusher/slider state
             old_pusher = pusher.copy()
             pusher[0] += j[3]*max_avel*dt
-            pusher[0] = numpy.clip(pusher[0], -0.5*box_width, 0.5*box_width)
+            pusher[0] = numpy.clip(pusher[0], -0.5*slider_width, 0.5*slider_width)
             new_pusher_angle = numpy.arctan2(pusher[1], pusher[0])
             angle_vel = (new_pusher_angle - pusher_angle)/dt
             pusher_angle = new_pusher_angle
-
-            x = numpy.array([box.pos[0], box.pos[1], box.heading, pusher_angle])
+            x = numpy.array([slider.pos[0], slider.pos[1], slider.heading, pusher_angle])
             fnc = -fmax*numpy.clip(j[1], -1, 0)
             ftc = numpy.clip(-fmax*j[0], -mu_g*fnc, mu_g*fnc)
             u = numpy.array([fnc, ftc, angle_vel])
-
             x += dt*xdot(x, u, pusher, side)
-
-            box.update(x[:2], x[2])
-
-            R = Rotation.from_euler('z', box.heading, degrees=False).as_matrix()[:2,:2]
-            pusher_in_world = R@pusher + box.pos
+            slider.update(x[:2], x[2])
+            R = Rotation.from_euler('z', slider.heading, degrees=False).as_matrix()[:2,:2]
+            pusher_in_world = R@pusher + slider.pos
 
             # Draw
             screen.reset()
-            screen.windows['robotenv'].draw_box('red', box.width, box.height, box.pos, box.heading)
+            screen.windows['robotenv'].draw_box('red', slider.width, slider.height, slider.pos, slider.heading)
             screen.windows['robotenv'].robots['pusher'].draw(pusher_in_world)
             screen.final()
             clock.tick_busy_loop(hz)
